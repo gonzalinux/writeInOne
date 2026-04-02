d3abf835f4973f57c9625c5470b39c92dd7c07eb
gonzalinux <gonzaleonrey@hotmail.com>
2026-04-02 18:32:55 +0200

    Does this work

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,10 +3,12 @@ package com.gonzalinux
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import reactor.core.publisher.Hooks
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class Application
 fun main(args: Array<String>) {
+    Hooks.enableAutomaticContextPropagation()
     runApplication<Application>(*args)
 }
